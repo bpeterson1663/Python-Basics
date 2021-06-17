455d990c82b2a30699d6ce1fcebb4ea992d3c26b
Brady Peterson <bpeterson1663@gmail.com>
2021-06-17 02:17:07 +0000

    feat(data types): working with ints and floats

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
-name = input("What is your name?")
-print("hello: " + name)
+# name = input("What is your name?")
+# print("hello: " + name)
+
+#Fundamental Data Types
+#int
+print(type(6))
+#float
+print(type(2 / 5))
+print(type(2 * .333))
+print(2 * .5)
+
+print(2 ** 3) #2 to the power of 3
+print(6 // 4) # 1 removes remainder
+print( 6 % 4) # 2
+
+#Math functions
+print(round(3.567, 2)) # 3.57
+
+print(abs(-20))
+bool
+str
+list
+tuple
+set
+dict
+
+#Classes -> custom types
+
+#Specialized Data Types
+
+None
