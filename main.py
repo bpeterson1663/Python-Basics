447d66f692b4fa79ac4aa1bdb6ff22070e846ff1
Brady Peterson <bpeterson1663@gmail.com>
2021-06-22 12:35:53 +0000

    feat(inputs): password checker assignment

--- main.py
+++ main.py
@@ -66,4 +66,11 @@ dict
 #Specialized Data Types
 
 None
+print('*' * 10)
+#password checker assignment
+username = input('enter your username')
+password = input('enter your password')
+password_length = len(password)
+hidden_password = '*' * password_length
+print(f'{username}, your password {hidden_password} is {password_length} letters long')
 
