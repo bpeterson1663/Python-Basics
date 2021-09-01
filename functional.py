e82535686c376e92a36f1081d9402b2599cfb4e3
Brady Peterson <bpeterson1663@gmail.com>
2021-08-31 21:48:41 -0500

    feat(functional): functional programming with map

---
+++ functional.py
@@ -0,0 +1,21 @@
+# Pure Function
+def multiply_by2(li):
+    new_list = []
+    for item in li:
+        new_list.append(item * 2)
+    return new_list
+
+
+print(multiply_by2([1, 2, 3]))
+
+# map
+
+my_list = [1, 2, 3]
+
+
+def multiply_by2_modified(item):
+    return item * 2
+
+
+print(list(map(multiply_by2_modified, my_list)))
+print(my_list)  # my_list is unaffected
