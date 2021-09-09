a3ef4de7ac393945e99c7d3390796c687bfc3a73
Brady Peterson <bpeterson1663@gmail.com>
2021-09-08 21:22:31 -0500

    feat(decorators): working with decorators

---
+++ decorators.py
@@ -0,0 +1,24 @@
+# Decorator
+
+def my_decorator(func):
+    def wrap_func():
+        print("*******")
+        func()
+        print("*******")
+    return wrap_func
+
+
+@my_decorator
+def hello():
+    print('hello')
+
+
+@my_decorator
+def bye():
+    print('bye')
+
+
+hello()
+bye()
+
+
