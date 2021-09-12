f14bd4622709a1866d379216fbdd9c21c8dd2b05
Brady Peterson <bpeterson1663@gmail.com>
2021-09-12 12:18:55 -0500

    feat(generator): working with generators

---
+++ generators.py
@@ -0,0 +1,47 @@
+from decorators import performance
+
+
+@performance
+def generator_function(num):
+    for i in range(num):
+        yield i
+
+
+for item in generator_function(1000):
+    print(item)
+
+
+def special_for(iterable):
+    iterator = iter(iterable)
+    while True:
+        try:
+            print(iterator)
+            next(iterator)
+        except StopIteration:
+            break
+
+
+special_for([1, 2, 3])
+
+
+class MyGen():
+    current = 0
+
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if MyGen.Current < self.last:
+            num = MyGen.current
+            MyGen.current += 1
+            return num
+        raise StopIteration
+
+
+gen = MyGen(0, 100)
+for i in gen:
+    print(i)
