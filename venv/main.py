19fbf4a154a404b3218b4e5ce7552d2d25c9d246
Brady Peterson <bpeterson1663@gmail.com>
2021-09-14 21:32:02 -0500

    feat(modules): working with built in modules, random

--- venv/main.py
+++ venv/main.py
@@ -1,6 +1,14 @@
 import utility
+from random import random, randint, choice, shuffle
 from shopping.shopping_cart import buy
 
 print(buy('item'))
 
 print(utility.multiply(7, 6))
+
+print(random())
+print(randint(1, 100))
+print(choice([2, 4, 6, 8]))
+my_list = [1, 2, 3, 4, 5, 6]
+shuffle(my_list)
+print(my_list)
