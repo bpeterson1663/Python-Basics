9bfab2ceb463c16397134a6000e896117f2492e5
Brady Peterson <bpeterson1663@gmail.com>
2021-09-25 00:24:19 -0500

    feat(regexp): password validation

--- venv/regexp.py
+++ venv/regexp.py
@@ -1,9 +1,11 @@
 import re
 
-pattern = re.compile('this')
+pattern = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
 string = 'search for this inside of this text please!'
 
-a = pattern.search(string)
+password = ""
+reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,18}$"
 
-print(a.span())
-print(a.group())
+passwordPattern = re.compile(reg)
+check = passwordPattern.fullmatch(password)
+print(check)
