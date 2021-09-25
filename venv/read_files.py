86727e9d139f4972102c97dd9d1b35bb4af7a22a
Brady Peterson <bpeterson1663@gmail.com>
2021-09-24 21:25:11 -0500

    feat(files): io errors

--- venv/read_files.py
+++ venv/read_files.py
@@ -1,3 +1,7 @@
-with open('text.txt', mode='r+') as my_file:
-    text = my_file.write('hey this is a test')
-    print(my_file.readlines())
+try:
+    with open('text.txt', mode='r+') as my_file:
+        text = my_file.write('hey this is a test')
+        print(my_file.readlines())
+except IOError as err:
+    print('IOError')
+    raise err
