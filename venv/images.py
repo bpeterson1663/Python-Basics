7a92e5d81428355573d18485e08725c37c4ad3d2
Brady Peterson <bpeterson1663@gmail.com>
2021-09-29 21:19:01 -0500

    feat(images): working with save and thmbnail

--- venv/images.py
+++ venv/images.py
@@ -3,10 +3,15 @@ from PIL import Image, ImageFilter
 img = Image.open('./Pokedex/pikachu.jpg')
 filtered_img = img.filter(ImageFilter.EMBOSS)
 greyed_img = img.convert("L")
+filtered_img.rotate(90)
+
 greyed_img.save("greyed.png", "png")
 filtered_img.save("blur.png", "png")
 print(img)
 print(img.format)
 print(img.size)
 print(img.mode)
+filtered_img.show()
 
+filtered_img.thumbnail((400, 400))
+filtered_img.save('thumbnail.jpg')
