e68822511d4c98c948876dd5d5d3ed580918f71e
Brady Peterson <bpeterson1663@gmail.com>
2021-10-03 22:19:15 -0500

    feat(pdfs): working with merging pdfs and adding a watermark

--- venv/pdf.py
+++ venv/pdf.py
@@ -1,10 +1,23 @@
 import PyPDF2
 
-with open('dummy.pdf', 'rb') as file:
-    reader = PyPDF2.PdfFileReader(file)
-    page = reader.getPage(0)
-    page.rotateCounterClockwise(90)
-    writer = PyPDF2.PdfFileWriter()
-    writer.addPage(page)
-    with open('tilt.pdf', 'wb') as new_file:
-        writer.write(new_file)
+# with open('dummy.pdf', 'rb') as file:
+#     reader = PyPDF2.PdfFileReader(file)
+#     page = reader.getPage(0)
+#     page.rotateCounterClockwise(90)
+#     writer = PyPDF2.PdfFileWriter()
+#     writer.addPage(page)
+#     with open('tilt.pdf', 'wb') as new_file:
+#         writer.write(new_file)
+
+template = PyPDF2.PdfFileReader(open('tilt.pdf', 'rb'))
+watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
+
+output = PyPDF2.PdfFileWriter()
+
+for i in range(template.getNumPages()):
+    page = template.getPage(i)
+    page.mergePage(watermark.getPage(0))
+    output.addPage(page)
+
+    with open('watermarked_output.pdf', 'wb') as file:
+        output.write(file)
