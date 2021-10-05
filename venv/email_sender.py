0e97b4a33262b73d10c3807464e807c2f1bce7d0
Brady Peterson <bpeterson1663@gmail.com>
2021-10-04 22:30:23 -0500

    feat(emails): sending emails with templates

--- venv/email_sender.py
+++ venv/email_sender.py
@@ -1,12 +1,15 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Brady Peterson'
 email['to'] = 'bpeterson1663@gmail.com'
 email['subject'] = 'You won!'
 
-email.set_content('I am a python developer!')
+email.set_content(html.substitute({"name": "Tin Tin"}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
