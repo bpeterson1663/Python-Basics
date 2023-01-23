b9f316171ab0b0f153d7afaeac37dfa60521a4c1
Brady Peterson <bpeterson1663@gmail.com>
2023-01-23 07:35:52 -0600

    feature(io): working within io

---
+++ Basics/file_io.py
@@ -0,0 +1,11 @@
+with open('new_file.txt', mode='w') as my_file:
+    test =  my_file.write("adding to the end")
+# print(my_file.read())
+# print(my_file.readline())
+# print(my_file.readline())
+# print(my_file.readline())
+# print(my_file.readline())
+# print(my_file.readline())
+# print(my_file.readline())
+
+
