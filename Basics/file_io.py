a06272a56cef481660bac07033557b5dc049f4e2
Brady Peterson <bpeterson1663@gmail.com>
2023-01-23 07:41:36 -0600

    feature(io): error handling

--- Basics/file_io.py
+++ Basics/file_io.py
@@ -1,5 +1,4 @@
-with open('new_file.txt', mode='w') as my_file:
-    test =  my_file.write("adding to the end")
+
 # print(my_file.read())
 # print(my_file.readline())
 # print(my_file.readline())
@@ -8,4 +7,12 @@ with open('new_file.txt', mode='w') as my_file:
 # print(my_file.readline())
 # print(my_file.readline())
 
-
+try:    
+    with open('new_file.txt', mode='r') as my_file:
+        test =  my_file.write("adding to the end")
+except FileNotFoundError as err:
+    print('file does not exist')
+    raise err
+except IOError as err:
+    print('IO error')
+    raise err
