fa878054c8666ad14926c075172826b30795fc13
Brady Peterson <bpeterson1663@gmail.com>
2023-02-02 06:58:00 -0600

    feat(images): more image stuff

--- Excercise/pokedex/images.py
+++ Excercise/pokedex/images.py
@@ -5,4 +5,7 @@ img = Image.open('./pokemon/pikachu.jpg')
 filtered_img = img.filter(ImageFilter.EMBOSS)
 
 filtered_img.save("./pokemon/embos.png", "png")
+greyed_image = img.convert("L").resize((300,300))
 
+filtered_img.show()
+greyed_image.show() 
